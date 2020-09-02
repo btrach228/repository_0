799cc9239f60ee3a94d9268278c6aeb6b41fc141
bogdan <bogdantrach228@gmail.com>
2020-09-02 23:52:10 +0300

    myprojects

---
+++ MYPROJECTS/pepsiProject/script/main.js
@@ -0,0 +1,9 @@
+function imgSlider(anything) {
+    document.querySelector('.pepsi').src = anything;
+}
+
+function changebgColor(color) {
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+
+}
